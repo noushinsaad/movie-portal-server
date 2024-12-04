2dffa0de3291c1d7f0a10228904a99e44d37856f
Md. Noushin Islam <noushinshoha@gmail.com>
2024-12-04 18:08:36 +0600

    getting information by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        app.get('/movies/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await movieCollection.findOne(query);
+            res.send(result);
+        })
 
         app.post('/movies', async (req, res) => {
             const newMovie = req.body;
